9b5e35511172f4f51c8c5c4f0b1724501cc79516
gregorioeric <gregorio.eric@gmail.com>
2025-08-26 09:01:05 -0300

    Modificado o arquivo alunos_controllers

--- controllers/alunos_controllers.py
+++ controllers/alunos_controllers.py
@@ -17,27 +17,6 @@ def select_aluno_by_id(id_aluno):
     if aluno["id_aluno"] == id_aluno:
       return aluno
   return False
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def select_aluno_by_id(id_aluno):
-#   alunos = load_alunos()
-
-#   for aluno in alunos:
-#     if aluno["id_aluno"] == id_aluno:
-#       return aluno
-#   return False
 
 def select_aluno_por_email(email):
   alunos = load_alunos()
@@ -68,35 +47,3 @@ def insert_aluno(data_aluno):
   with open(ALUNOS, "w", encoding="utf-8") as arq_json:
     json.dump(alunos, arq_json, indent=4, ensure_ascii=False)
   return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def update_aluno_by_id(id, update_aluno):
-#   alunos = load_alunos()
-
-#   for aluno in alunos:
-#     if aluno["id_aluno"] == id:
-#       aluno["nome_aluno"] = update_aluno["nome_aluno"]
-#       return alunos
-#   return False
